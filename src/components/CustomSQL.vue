d13859f7ae3d739b6cdeec3ebfd469107deb422f
liuning <liuning@jycloud.tech>
2023-02-03 20:20:17 +0800

    临时保存

--- src/components/CustomSQL.vue
+++ src/components/CustomSQL.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="action-bar">
       <el-tooltip :show-after="1000" content="执行当前输入的SQL (Ctrl+Enter)" placement="top">
-        <el-icon class="icons" @click="execSql">
+        <el-icon class="icons" @click="execCurrentSql">
           <CaretRight/>
         </el-icon>
       </el-tooltip>
@@ -24,25 +24,10 @@
     </div>
     <splitpanes :dbl-click-splitter="false" class="custom-area" horizontal>
       <pane class="sql-content-pane" max-size="80" min-size="5" size="40">
-        <!--        <div ref="sqlContent" class="sql-content" contenteditable="true"-->
-        <!--             @mouseup="rememberSelection"-->
-        <!--             @keydown.ctrl.enter="execSql" @keydown.ctrl.shift.f="formatSql"-->
-        <!--             @keydown.ctrl.shift.b="uncommentLine" @keydown.ctrl.b="commentLine"-->
-        <!--        ></div>-->
-        <!--        <splitpanes :dbl-click-splitter="false">-->
-        <!--          <pane max-size="90" min-size="10" size="50">-->
-        <!--            <Toolbar :default-config="toolbarConfig" :editor="editorRef"></Toolbar>-->
-        <!--            <Editor v-model="sqlHtml" :default-config="editorConfig" mode="simple"-->
-        <!--                    @onChange="handleChange" @onCreated="handleCreated"-->
-        <!--                    @keydown.ctrl.enter="execCurrentSql"></Editor>-->
-        <!--          </pane>-->
-        <!--          <pane>-->
-        <!--            <highlightjs ref="hlSqlBlock" :code="sqlText" class="echo-sql-pre"></highlightjs>-->
-        <!--          </pane>-->
-        <!--        </splitpanes>-->
         <Toolbar :default-config="toolbarConfig" :editor="editorRef"></Toolbar>
         <Editor v-model="sqlHtml" :default-config="editorConfig" mode="simple"
-                @onChange="handleChange" @onCreated="handleCreated"
+                @customPaste="customPaste" @onCreated="handleCreated"
+                @keydown.ctrl.b="commentLine" @keydown.ctrl.shift.b="uncommentLine"
                 @keydown.ctrl.enter="execCurrentSql"></Editor>
       </pane>
       <pane size="60">
@@ -71,7 +56,7 @@ import {Pane, Splitpanes} from 'splitpanes'
 import 'splitpanes/dist/splitpanes.css'
 import '@wangeditor/editor/dist/css/style.css'
 import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
-import {IEditorConfig, IToolbarConfig} from "@wangeditor/editor";
+import {IEditorConfig, IToolbarConfig, SlateEditor, SlateElement, SlateNode} from "@wangeditor/editor";
 import {AlertType} from "@wangeditor/core/dist/core/src/config/interface"
 import {IDomEditor} from "@wangeditor/core/dist/core/src/editor/interface";
 import _ from 'lodash'
@@ -88,10 +73,8 @@ const sqlSelection = reactive<SqlSelection>({
   toLineNum: -1,
 });
 
-const hlSqlBlock = ref();
 const editorRef = shallowRef();
 const sqlHtml = ref('');
-const sqlText = ref('');
 const toolbarConfig: Partial<IToolbarConfig> = {
   toolbarKeys: ['fontSize', 'fontFamily', 'lineHeight', 'codeBlock']
 };
@@ -112,6 +95,12 @@ const editorConfig: Partial<IEditorConfig> = {
 const handleCreated = (editor: IDomEditor) => {
   editorRef.value = editor // 记录 editor 实例，重要！
 }
+/**
+ * 自定义粘贴处理方式，去除来源样式，仅保留文字。
+ * @param editor
+ * @param event
+ * @param callback
+ */
 const customPaste = (editor: IDomEditor, event: ClipboardEvent, callback: Function) => {
   if (event.clipboardData) {
     // const html = event.clipboardData.getData('text/html') // 获取粘贴的 html
@@ -130,106 +119,14 @@ const customPaste = (editor: IDomEditor, event: ClipboardEvent, callback: Functi
   }
 }
 
-const handleChange = (editor: IDomEditor) => {
-  onChange(editor);
-}
-
-const onChange = _.debounce((editor: IDomEditor) => {
-  sqlText.value = editor.getText();
-}, 50);
-
-
+/**
+ * 执行光标所在位置的SQL语句。
+ */
 const execCurrentSql = () => {
-  let editor: IDomEditor = editorRef.value as IDomEditor;
-  let selection = editor.selection;
-  if (selection?.anchor !== undefined) {
-    let fromLineNum: number | undefined = selection.anchor.path[0];
-    let sql = '';
-    if (_.isEqual(selection.focus, selection.anchor)) {
-      // 未选择任何文本
-      // 计算光标的绝对字符位置
-      let text = editor.getText();
-      console.log(text)
-
-      let ahead = 0;
-      // 计算光标之前所有行的字符串长度，不包括光标所在行
-      for (let i = 0, lastRetPos = 0; i < fromLineNum; i++) {
-        lastRetPos = text.indexOf('\n', ahead);
-        if (lastRetPos != -1) {
-          ahead = lastRetPos + 1;
-        }
-      }
-      ahead += selection.anchor.offset;
-      
-      // 处理从开始到光标处的字符串
-      let realStart = 0, realEnd = text.length;
-      let preHalf = text.substring(0, ahead).trimEnd();
-      if (selection.anchor.offset != 0 && preHalf.charAt(preHalf.length - 1) == ';') {
-        preHalf = preHalf.substring(0, preHalf.length - 1);
-        /*
-         光标在分号后面，最近的SQL语句起始位置在最近的两个分号之间，
-         或从0开始到前面最近一个分号处。
-         */
-        let lastSemiPos = preHalf.lastIndexOf(';');
-        if (lastSemiPos != -1) {
-          realStart = lastSemiPos + 1;
-        }
-        sql = preHalf.substring(realStart).trim();
-      } else {
-        // 光标在语句中间
-        preHalf = text.substring(0, ahead);
-        let lastSemiPos = preHalf.lastIndexOf(';');
-        if (lastSemiPos != -1) {
-          realStart = lastSemiPos + 1;
-        }
-        let firstSemiPos = text.indexOf(';', ahead);
-        if (firstSemiPos != -1) {
-          realEnd = firstSemiPos;
-        }
-        sql = text.substring(realStart, realEnd).trim();
-      }
-    } else {
-      sql = editor.getSelectionText().trim();
-    }
-    console.log('sql', sql.trim().replaceAll('\n', ' '));
-  }
-}
-
-onBeforeUnmount(() => {
-  const editor = editorRef.value;
-  if (editor) {
-    editor.destroy();
-  }
-})
-
-
-const dataState = reactive({
-  arrayResult: [] as any[],
-  actionResult: '',
-  arrayResultCols: new Set<string>(),
-});
-const lastExecOnDbPath = ref('');
-
-emitter.on('meta_objects_refreshed', (newCurrent) => {
-  pageCache.current = newCurrent as CurrentDbAndTable;
-});
-
-const watermarkText = ref('');
-const execSql = () => {
-  if (pageCache.current.db !== undefined) {
+  if (pageCache.current.db) {
     lastExecOnDbPath.value = pageCache.current.db;
-
-    /*
-    重组SQL语句，忽略注释行，合并冗余的空白和换行符。
-     */
-    let normalizedSql = '';
-    let origLines: string[] = sqlContent.value.innerText.trim().split('\n');
-    for (let i = 0; i < origLines.length; i++) {
-      if (!origLines[i].trim().startsWith('--')) {
-        normalizedSql += origLines[i] + ' ';
-      }
-    }
-    normalizedSql = normalizedSql.replaceAll(/\s+/g, ' ')
+    let editor: IDomEditor = editorRef.value as IDomEditor;
+    let normalizedSql = extractSql(editor);
 
     /*
     组织调用参数。
@@ -274,6 +171,123 @@ const execSql = () => {
   }
 }
 
+/**
+ * 从选区或光标所在位置优先向前查找完整的SQL语句，若不完整则追加向后查找到完整的语句。
+ * 在光标前后两个分号之间的字符串被认为是完整的SQL语句。
+ * 或者在贴近光标前的分号(忽略光标到分号之间的空白)到编辑区头部的字符串也被当作完整的SQL，
+ * 最后或者从光标前的最近一个分号或区首，到光标后的一个分号或区末，被当作一个SQL。
+ * @param editor
+ */
+const extractSql = (editor: IDomEditor): string => {
+  let sql = '';
+  let selection = editor.selection;
+  if (selection && _.isEqual(selection.focus, selection.anchor)) {
+    /*
+    未选择任何文本，需要计算光标所在位置的一维坐标。
+     */
+    let text = editor.getText();
+
+    /*
+    设置新选区：从当前光标位置到编辑区起始0坐标处，
+    再根据新选区内的文本长度，计算出当前光标的一维坐标。
+     */
+    let newSelection = {
+      anchor: selection.anchor,
+      focus: {
+        path: [0, 0],
+        offset: 0
+      }
+    };
+    editor.select(newSelection);
+    let prePart = editor.getSelectionText();
+    // 当前光标的一维坐标
+    let ahead = prePart.length;
+
+    {
+      /*
+      BUG修复代码段，在目前的wangedit版本
+      "@wangeditor/editor": "^5.1.23",
+      "@wangeditor/editor-for-vue": "^5.1.12"
+      中，getSelectionText()会有漏掉换行符的BUG，
+      这里专门进行修正。
+      以后的wangeditor版本或是修复了该BUG，则可以考虑去掉这块修复代码。
+       */
+      ahead += selection.anchor.path[0];
+    }
+
+    // 处理从开始到光标处的字符串
+    let realStart = 0, realEnd = text.length;
+    let preHalf = text.substring(0, ahead).trimEnd();
+    if (selection.anchor.offset != 0 && preHalf.charAt(preHalf.length - 1) == ';') {
+      preHalf = preHalf.substring(0, preHalf.length - 1);
+      /*
+       光标在分号后面，最近的SQL语句起始位置在最近的两个分号之间，
+       或从0开始到前面最近一个分号处。
+       */
+      let lastSemiPos = preHalf.lastIndexOf(';');
+      if (lastSemiPos != -1) {
+        realStart = lastSemiPos + 1;
+      }
+      sql = text.substring(realStart, ahead);
+    } else {
+      // 光标在语句中间
+      preHalf = text.substring(0, ahead);
+      let lastSemiPos = preHalf.lastIndexOf(';');
+      if (lastSemiPos != -1) {
+        realStart = lastSemiPos + 1;
+      }
+      let firstSemiPos = text.indexOf(';', ahead);
+      if (firstSemiPos != -1) {
+        realEnd = firstSemiPos;
+      }
+      sql = text.substring(realStart, realEnd);
+    }
+
+    editor.deselect();
+    //editor.focus();
+  } else {
+    // 选中了一些文本
+    sql = editor.getSelectionText();
+  }
+  return normalizeSql(sql);
+}
+/**
+ * 去除字符串中的换行符和冗余的空白，保留行首的注释符号。
+ * @param sql
+ */
+const normalizeSql = (sql: string): string => {
+  let normalizedSql = '';
+  let origLines: string[] = sql.split('\n');
+  for (let i = 0; i < origLines.length; i++) {
+    if (!origLines[i].trim().startsWith('--')) {
+      normalizedSql += origLines[i] + ' ';
+    }
+  }
+  normalizedSql = normalizedSql.replaceAll(/\s+/g, ' ').trim();
+  if (normalizedSql.charAt(normalizedSql.length - 1) == ';') {
+    normalizedSql = normalizedSql.substring(0, normalizedSql.length - 1);
+  }
+  return normalizedSql;
+}
+
+onBeforeUnmount(() => {
+  const editor = editorRef.value;
+  if (editor) {
+    editor.destroy();
+  }
+})
+
+
+const dataState = reactive({
+  arrayResult: [] as any[],
+  actionResult: '',
+  arrayResultCols: new Set<string>(),
+});
+const lastExecOnDbPath = ref('');
+
+emitter.on('meta_objects_refreshed', (newCurrent) => {
+  pageCache.current = newCurrent as CurrentDbAndTable;
+});
 
 const formatSql = () => {
   sqlContent.value.innerText = format(sqlContent.value.innerText.trim(), {
@@ -288,57 +302,86 @@ const formatSql = () => {
  * 注释光标所在行或选中的多个行。
  */
 const commentLine = (e: Event) => {
+  console.log('开始添加注释')
+
+  let editor = editorRef.value as IDomEditor;
+  let selection = editor.selection;
+  let nodeEntries = SlateEditor.nodes(editor, {
+    match: (node: SlateNode) => {
+      if (SlateElement.isElement(node)) {
+        console.log('节点类型', node)
+        debugger
+        if (node.type == 'paragraph') {
+          return true;
+        }
+      }
+      return false;
+    },
+    universal: true
+  });
+  if (nodeEntries) {
+    for (let nodeEntry of nodeEntries) {
+      const [node, path] = nodeEntry
+      console.log('选中了 paragraph 节点', node)
+      console.log('节点 path 是', path)
+    }
+  } else {
+    console.log('当前未选中的 paragraph')
+  }
+
   /*
   解决快捷键多重激发的问题：约定当前函数的快捷键为`Ctrl+b`，
   当按下`Ctrl+Shift+b`时也会触发本方法，所以需要进行检测拦截。
    */
-  if (e instanceof KeyboardEvent && (e as KeyboardEvent).shiftKey) {
-    return;
-  }
+  // if (e instanceof KeyboardEvent && (e as KeyboardEvent).shiftKey) {
+  //   return;
+  // }
 
-  let sqlDiv = sqlContent.value;
+  // let sqlDiv = sqlContent.value;
+  // let editor: IDomEditor = editorRef.value as IDomEditor;
+  // let sqlDiv = editor.getEditableContainer();
 
   /*
   根据当前光标所在位置，或所选区域，计算出将要添加注释字符的行。
    */
-  let fromLineNum = -1, toLineNum = -1;
-  if (e instanceof MouseEvent) {
-    fromLineNum = sqlSelection.fromLineNum;
-    toLineNum = sqlSelection.toLineNum;
-    console.log(fromLineNum, toLineNum)
-  } else {
-    let s = window.getSelection();
-    if (s) {
-      let selected = calcSelectionRange(sqlDiv, s);
-      fromLineNum = selected.min;
-      toLineNum = selected.max;
-
-      s.collapseToEnd();
-    }
-  }
+  // let fromLineNum = -1, toLineNum = -1;
+  // if (e instanceof MouseEvent) {
+  //   fromLineNum = sqlSelection.fromLineNum;
+  //   toLineNum = sqlSelection.toLineNum;
+  //   console.log(fromLineNum, toLineNum)
+  // } else {
+  //   let s = window.getSelection();
+  //   if (s) {
+  //     let selected = calcSelectionRange(sqlDiv, s);
+  //     fromLineNum = selected.min;
+  //     toLineNum = selected.max;
+  //
+  //     s.collapseToEnd();
+  //   }
+  // }
 
   /*
   在目标行开头处添加"--"字符串。
    */
-  if (toLineNum > -1) {
-    if (fromLineNum > -1) {
-      for (let i = fromLineNum; i <= toLineNum; i++) {
-        let n = sqlDiv.childNodes[i];
-        if (n.nodeName == '#text') {
-          n.nodeValue = '--' + n.nodeValue;
-        } else {
-          n.innerText = '--' + n.innerText;
-        }
-      }
-    } else {
-      let n = sqlDiv.childNodes[toLineNum];
-      if (n.nodeName == '#text') {
-        n.nodeValue = '--' + n.nodeValue;
-      } else {
-        n.innerText = '--' + n.innerText;
-      }
-    }
-  }
+  // if (toLineNum > -1) {
+  //   if (fromLineNum > -1) {
+  //     for (let i = fromLineNum; i <= toLineNum; i++) {
+  //       let n = sqlDiv.childNodes[i];
+  //       if (n.nodeName == '#text') {
+  //         n.nodeValue = '--' + n.nodeValue;
+  //       } else {
+  //         n.innerText = '--' + n.innerText;
+  //       }
+  //     }
+  //   } else {
+  //     let n = sqlDiv.childNodes[toLineNum];
+  //     if (n.nodeName == '#text') {
+  //       n.nodeValue = '--' + n.nodeValue;
+  //     } else {
+  //       n.innerText = '--' + n.innerText;
+  //     }
+  //   }
+  // }
 }
 
 /**
@@ -394,25 +437,6 @@ const uncommentLine = (e: Event) => {
   }
 }
 
-
-/**
- * 编辑框失去焦点时，记录选区位置。
- */
-const rememberSelection = function (e: MouseEvent) {
-  // console.log('开始处理选区');
-
-  let sqlDiv = sqlContent.value;
-  let s = window.getSelection();
-
-  if (s) {
-    let selected = calcSelectionRange(sqlDiv, s);
-    sqlSelection.fromLineNum = selected.min;
-    sqlSelection.toLineNum = selected.max;
-    // 这里不能收缩选区，否则会导致无法选择的BUG。
-  }
-  // console.log('完成处理选区', sqlSelection.fromLineNum, sqlSelection.toLineNum);
-}
-
 /**
  * 计算DIV选区的起始行号和结束行号，若未选择则给出光标所在行号。
  * @param sqlDiv SQL编辑区的DIV对象。
@@ -433,31 +457,6 @@ const calcSelectionRange = (sqlDiv: any, s: Selection): SelectedLines => {
     min: Math.min(y1, y2),
     max: Math.max(y1, y2),
   }
-}
-
-const trimTagOnPaste = (e: Event) => {
-  // e.preventDefault();
-  // let text = '';
-  // let ce = e as ClipboardEvent;
-  // let clp = ce.clipboardData;
-  // if (clp) {
-  //   text = clp.getData('text/plain') || '';
-  //   if (text) {
-  //     let coloredText = highlight(text, {html: true});
-  //     console.log(coloredText)
-  //     sqlContent.value.innerHTML = coloredText;
-  //     // let newText = highlight(text, {html: true});
-  //     let s = window.getSelection();
-  //     if (s) {
-  //       let nodes = sqlContent.value.childNodes;
-  //       let node = nodes[nodes.length - 1];
-  //       let r = s.getRangeAt(0);
-  //       r.selectNodeContents(node);
-  //       s.collapse(node, text.length);
-  //     }
-  //   }
-  // }
-
 }
 
 </script>
@@ -489,19 +488,6 @@ const trimTagOnPaste = (e: Event) => {
   border-radius: 5px;
 }
 
-.sql-content {
-  font-family: monospace;
-  font-size: 1.1em;
-  background-color: floralwhite;
-  width: 100%;
-  height: 100%;
-  padding: .5em;
-  overflow: scroll;
-  border-radius: 5px;
-  box-shadow: 0 0 10px #000000 inset;
-  /*box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1), 0 0 8px rgba(82, 168, 236, 0.6);*/
-}
-
 .last-db-path {
   margin-top: 1em;
   color: lightslategray;
@@ -514,17 +500,4 @@ const trimTagOnPaste = (e: Event) => {
 .result-table {
   width: 100%;
 }
-
-.echo-sql-pre {
-  height: 97%;
-  width: 100%;
-  overflow: scroll;
-  margin-top: 0;
-  white-space: pre-wrap;
-  word-wrap: break-word;
-}
-
-/*.echo-sql-pre > code {*/
-/*  min-height: 88%;*/
-/*}*/
 </style>
